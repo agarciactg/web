3c8edc3ed1f7f3a56ae47a4e99daac52c04a2e3e
Andres Garcia <agarciactg@gmail.com>
2024-05-03 23:22:30 -0500

    configuracion correo.

--- web/config/settings/base.py
+++ web/config/settings/base.py
@@ -291,7 +291,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'agarciacompanyctg@gmail.com'
-EMAIL_HOST_PASSWORD = 'uhji srjp qjkn hwwz'
+EMAIL_HOST_PASSWORD = 'vhhj msei japa omgp'
 
 
 ANYMAIL = {
